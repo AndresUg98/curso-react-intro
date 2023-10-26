b88bc84dd5e769c2a42fc3016f53beaa211d8985
AndresUg98 <oscaran2109@gmail.com>
2023-10-26 13:42:35 -0600

    counting todos

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import { TodoSearch } from "./TodoSearch";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
-import React from "react";
+import React, { useState } from "react";
 
 const defaultTodos = [
   { title: "Cortar cebolla", completed: true },
   { title: "Tomar curso Intro react.js", completed: false },
   { title: "Llorar con la llorona", completed: false },
   { title: "Ser el mejor", completed: true },
+  { title: "Usar estados derivados", completed: true },
 ];
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const [todos, setTodos] = useState(defaultTodos);
+
+  //Friltrando los todos que que han completado
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+
+  const totalTodos = todos.length;
+
   return (
     <React.Fragment>
       <div className="appCoontainer">
-        <TodoCounter completed={16} total={25} />
-        <TodoSearch />
+        <TodoCounter completed={completedTodos} total={totalTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <TodoList>
           {defaultTodos.map((todo) => (
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,17 @@
-import React from "react";
-import './TodoSearch.css'
+import "./TodoSearch.css";
 
-function TodoSearch() {
-  return <input placeholder="Cortar cebolla" className="searchInput"></input>;
+function TodoSearch({ searchValue, setSearchValue }) {
+  console.log(searchValue);
+  return (
+    <input
+      placeholder="Cortar cebolla"
+      className="searchInput"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    ></input>
+  );
 }
 
 export { TodoSearch };
