11dc7ccb5abb1d491f03ddc0d3cec9f4710c422d
AndresUg98 <oscaran2109@gmail.com>
2023-11-09 12:47:49 -0600

    fixing the css

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -1,7 +1,17 @@
 import "./EmptyTodos.css";
 
 function EmptyTodos() {
-  return <p>Crea tu primer todo</p>;
+  return (
+    <div className="Empty-Container">
+      <h2 className="Empty-title">¡Crea tu primer ToDo!</h2>
+      <figure className="Empty-ImgContainer">
+        <img
+          alt="a person working background image"
+          src="https://d11cuk1a0j5b57.cloudfront.net/blog/wp-content/uploads/2020/04/Trabajar-desde-casa-portada.jpg"
+        />
+      </figure>
+    </div>
+  );
 }
 
 export { EmptyTodos };
